becc3ce748ec365b457dd0346e17406fbd55f0d9
Rohit Nautiyal <85772545+Nauti-Rohit@users.noreply.github.com>
2024-03-11 23:30:40 +0530

    Update app.js
    fix 'root' Route problem

--- app.js
+++ app.js
@@ -65,9 +65,9 @@ const sessionOptions = {
   },
 };
 
-// app.get("/", (req, res) => {
-//   res.send("Hi! I am root");
-// });
+ app.get("/", (req, res) => {
+  res.redirect("/listings");
+ });
 
 app.use(session(sessionOptions));
 app.use(flash());
